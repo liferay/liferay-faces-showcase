bc5b9bb296c985fd46d7a9ed4d83ec800f9e7840
pcwhite <philip.white@liferay.com>
2016-12-09 22:18:20 -0500

    FACES-2977 Develop tests for the Portlet and Bridge:InputFile category of the JSF Showcase (Portlet)

--- jsf-showcase-webapp/src/test/java/com/liferay/faces/test/showcase/image/ImageGeneralTester.java
+++ jsf-showcase-webapp/src/test/java/com/liferay/faces/test/showcase/image/ImageGeneralTester.java
@@ -13,23 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.liferay.faces.test.showcase.multimediaimage;
+package com.liferay.faces.test.showcase.image;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import org.openqa.selenium.WebElement;
-
 import com.liferay.faces.test.selenium.Browser;
-import com.liferay.faces.test.selenium.assertion.SeleniumAssert;
-import com.liferay.faces.test.showcase.TesterBase;
 
 
 /**
  * @author  Kyle Stiemann
  * @author  Philip White
  */
-public class ImageGeneralTester extends TesterBase {
+public class ImageGeneralTester extends ImageTester {
 
 	@Test
 	public void runImageGeneralTest() throws Exception {
@@ -44,10 +39,10 @@ public class ImageGeneralTester extends TesterBase {
 		navigateToUseCase(browser, componentName, "general");
 
 		// Test that the images render on the page successfully.
-		assertImageRendered(browser, "value");
-		assertImageRendered(browser, "#{resource}");
-		assertImageRendered(browser, "name");
-		assertImageRendered(browser, "usemap");
+		assertImageRendered(browser, getImageXpath("value"));
+		assertImageRendered(browser, getImageXpath("#{resource}"));
+		assertImageRendered(browser, getImageXpath("name"));
+		assertImageRendered(browser, getImageXpath("usemap"));
 
 		// Click the image links on both areas of the example 4 image usemap and check that it opens a new window/tab
 		// with the correct domain name.
@@ -57,23 +52,9 @@ public class ImageGeneralTester extends TesterBase {
 			"jcp.org");
 	}
 
-	private void assertImageRendered(Browser browser, String imageExampleLabelText) {
+	private String getImageXpath(String imageExampleLabelText) {
+		return "//label[contains(.,'Example')][contains(.,'" + imageExampleLabelText +
+				"')]/ancestor::div[@class='showcase-example']//img[contains(@src,'javax.faces.resource')][contains(@src,'ln=images') or contains(@src,'ln:images')]";
 
-		String imageXpath = "//label[contains(.,'Example')][contains(.,'" + imageExampleLabelText +
-			"')]/ancestor::div[@class='showcase-example']//img[contains(@src,'javax.faces.resource')][contains(@src,'ln=images') or contains(@src,'ln:images')]";
-
-		SeleniumAssert.assertElementVisible(browser, imageXpath);
-
-		WebElement image = browser.findElementByXpath(imageXpath);
-		String imageSrc = image.getAttribute("src");
-		Assert.assertTrue("Image src " + imageSrc + " is not a valid JSF resource URL.",
-			imageSrc.matches(".*javax.faces.resource\\p{Punct}[a-z-]+[.]png.*") &&
-			imageSrc.matches(".*ln\\p{Punct}images.*"));
-
-		Boolean imageRendered = (Boolean) browser.executeScript(
-				"return arguments[0].complete && typeof arguments[0].naturalWidth != 'undefined' && arguments[0].naturalWidth > 0",
-				image);
-		Assert.assertTrue("Image " + imageXpath + " (src=\"" + imageSrc + "\") is not rendered in the DOM.",
-			imageRendered);
 	}
 }
